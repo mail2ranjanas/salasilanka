781686d9da7f94e08123ecfd850104fe24b862e7
ranjana_samaranayake <mail2ranjanas@gmail.com>
2020-06-03 23:42:58 +1000

    commit vendor

---
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+use Spatie\Permission\Models\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+use Log;
+
+trait RegistersUsers
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        $roles = Role::all();
+        echo($roles);
+       // $roles=$roles->pluck('name','id');
+        //$items = $items->pluck('itemCode', 'id');
+        return view('auth.register')->with('roles', $roles);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        
+
+        $user=User::find(1);
+
+        event(new Registered($user = $this->create($request->all())));
+        if(!empty($request->role)) {
+
+            //Assign Roles to user
+            foreach($request->role as $value){
+                Log::info($value);
+                $role=Role::find($value);
+                Log::info('role'.$role);
+                $user->assignRole($role);
+            }
+    
+        }
+        $this->guard()->login($user);
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        //
+    }
+}
